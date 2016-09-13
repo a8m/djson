7f7f811079aa7303e81d35527d34bd10b5ad05c4
Ariel Mashraki <ariel@mashraki.co.il>
2016-09-13 23:49:55 +0300

    add bench

--- benchmark/benchmark_fixture_test.go
+++ benchmark/benchmark_fixture_test.go
@@ -4,7 +4,11 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/Jeffail/gabs"
 	"github.com/a8m/djson"
+	"github.com/antonholmquist/jason"
+	"github.com/bitly/go-simplejson"
+	"github.com/mreiferson/go-ujson"
 )
 
 func BenchmarkEncodingJsonParser(b *testing.B) {
@@ -26,6 +30,85 @@ func BenchmarkEncodingJsonParser(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			data := make(map[string]interface{})
 			json.Unmarshal(largeFixture, &data)
+		}
+	})
+}
+
+func BenchmarkJasonParser(b *testing.B) {
+	b.Run("small", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			jason.NewObjectFromBytes(smallFixture)
+		}
+	})
+
+	b.Run("medium", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			jason.NewObjectFromBytes(mediumFixture)
+		}
+	})
+
+	b.Run("large", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			jason.NewObjectFromBytes(largeFixture)
+		}
+	})
+}
+
+func BenchmarkSimpleJsonParser(b *testing.B) {
+	b.Run("small", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			simplejson.NewJson(smallFixture)
+		}
+	})
+
+	b.Run("medium", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			simplejson.NewJson(mediumFixture)
+		}
+	})
+
+	b.Run("large", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			simplejson.NewJson(largeFixture)
+		}
+	})
+}
+func BenchmarkGabsParser(b *testing.B) {
+	b.Run("small", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			gabs.ParseJSON(smallFixture)
+		}
+	})
+
+	b.Run("medium", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			gabs.ParseJSON(mediumFixture)
+		}
+	})
+
+	b.Run("large", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			gabs.ParseJSON(largeFixture)
+		}
+	})
+}
+
+func BenchmarkUJsonParser(b *testing.B) {
+	b.Run("small", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ujson.NewFromBytes(smallFixture)
+		}
+	})
+
+	b.Run("medium", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ujson.NewFromBytes(mediumFixture)
+		}
+	})
+
+	b.Run("large", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ujson.NewFromBytes(largeFixture)
 		}
 	})
 }
