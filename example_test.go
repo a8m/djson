bf33aa94d6343123e6e179d141818d12dc0e32f2
Ariel Mashraki <ariel@mashraki.co.il>
2016-10-01 10:28:33 -0700

    example: add examples to docs

--- example_test.go
+++ example_test.go
@@ -17,7 +17,11 @@ func ExampleDecode() {
 	if err != nil {
 		log.Fatal("error:", err)
 	}
-	fmt.Printf("%#v", val)
+
+	fmt.Printf("%+v", val)
+
+	// Output:
+	// [map[Name:Platypus Order:Monotremata] map[Name:Quoll Order:Dasyuromorphia]]
 }
 
 func ExampleDecodeArray() {
@@ -37,22 +41,59 @@ func ExampleDecodeArray() {
 	}
 }
 
+// Example that demonstrate the basic transformation I do on each incoming
+// event.
+// `lowerKeys` and `§fixEncoding` are two generic methods, and they don't care
+// about the schema.
+// The three others(`maxMindGeo`, `dateFormat`and `refererURL`) process and
+// extend the events dynamically based on the "APP_ID" field.
 func ExampleDecodeObject() {
 	var data = []byte(`{
-		"id": 76523,
-		"name": "Ariel",
-		"username": "a8m",
-		"score": 99,
-		"image": {
-			"src": "images/67.png",
-			"height": 450,
-			"width":  370,
-			"alignment": "center"
-		}
+		"ID": 76523,
+		"IP": "69.89.31.226"
+		"APP_ID": "BD311",
+		"Name": "Ariel",
+		"Username": "a8m",
+		"Score": 99,
+		"Date": 1475332371532,
+		"Image": {
+			"Src": "images/67.png",
+			"Height": 450,
+			"Width":  370,
+			"Alignment": "center"
+		},
+		"RefererURL": "http://..."
 	}`)
-	user, err := djson.DecodeObject(data)
+	event, err := djson.DecodeObject(data)
 	if err != nil {
 		log.Fatal("error:", err)
 	}
-	fmt.Printf("User: %v", user)
+
+	fmt.Printf("Value: %v", event)
+
+	// Process the event
+	//
+	// lowerKeys(event)
+	// fixEncoding(event)
+	// dateFormat(event)
+	// maxMindGeo(event)
+	// refererURL(event)
+	//
+	// pipeline.Pipe(event)
+}
+
+func ExampleDecoder_AllocString() {
+	var data = []byte(`{"event_type":"click","count":"93","userid":"4234A"}`)
+	dec := djson.NewDecoder(data)
+	dec.AllocString()
+
+	val, err := dec.DecodeObject()
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+
+	fmt.Printf("Value: %+v", val)
+
+	// Output:
+	// map[count:93 userid:4234A event_type:click]
 }
